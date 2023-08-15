e8a86954fb49b11117776f6036d544d307c0875a
Han384 <dfu3896+infra@gmail.com>
2023-08-15 22:23:34 +0900

    01_VPC.tf： Subnetを追加

--- Terraform/01_VPC.tf
+++ Terraform/01_VPC.tf
@@ -13,3 +13,62 @@ resource "aws_vpc" "TerraformVPC" {
     Env     = var.environment
   }
 }
+
+# ---------------------------------------------
+# Subnet
+# ---------------------------------------------
+resource "aws_subnet" "PublicSubnet1a" {
+  vpc_id                  = aws_vpc.TerraformVPC.id
+  availability_zone       = "ap-northeast-1a"
+  cidr_block              = "10.0.0.0/20"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-public-subnet-1a"
+    Project = var.project
+    Env     = var.environment
+    Type    = "public"
+  }
+}
+
+resource "aws_subnet" "PublicSubnet1c" {
+  vpc_id                  = aws_vpc.TerraformVPC.id
+  availability_zone       = "ap-northeast-1c"
+  cidr_block              = "10.0.16.0/20"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-public-subnet-1c"
+    Project = var.project
+    Env     = var.environment
+    Type    = "public"
+  }
+}
+
+resource "aws_subnet" "PrivateSubnet1a" {
+  vpc_id                  = aws_vpc.TerraformVPC.id
+  availability_zone       = "ap-northeast-1a"
+  cidr_block              = "10.0.128.0/20"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-private-subnet-1a"
+    Project = var.project
+    Env     = var.environment
+    Type    = "private"
+  }
+}
+
+resource "aws_subnet" "PrivateSubnet1c" {
+  vpc_id                  = aws_vpc.TerraformVPC.id
+  availability_zone       = "ap-northeast-1c"
+  cidr_block              = "10.0.144.0/20"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-private-subnet-1c"
+    Project = var.project
+    Env     = var.environment
+    Type    = "private"
+  }
+}
